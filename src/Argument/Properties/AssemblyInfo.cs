0bb5a612a7a60bb835d6c3cb83a338728d9fb853
Matt Sharpe <matthew.sharpe@enable.com>
2017-07-31 11:55:41 +0100

    Add description to AssemblyInfo

--- src/Argument/Properties/AssemblyInfo.cs
+++ src/Argument/Properties/AssemblyInfo.cs
@@ -3,6 +3,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Enable.Common.Argument")]
+[assembly: AssemblyDescription("Enable.Common.Argument")]
 [assembly: AssemblyCompany("Enable · enable.com")]
 [assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: ComVisible(false)]
